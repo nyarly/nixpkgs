b0cd34f1e211d40a948dc8a67c8c4881fb0f7991
Judson <nyarly@gmail.com>
2018-10-10 13:45:21 -0700

    Just starting. It's complicated

---
+++ nixos/tests/dnscrypt-proxy2.nix
@@ -0,0 +1,32 @@
+import ./make-test.nix ({ pkgs, ... }: {
+  name = "dnscrypt-proxy2";
+  meta = with pkgs.stdenv.lib.maintainers; {
+    maintainers = [ nyarly ];
+  };
+
+  nodes = {
+    # A client running the recommended setup: DNSCrypt proxy as a forwarder
+    # for a caching DNS client.
+    client =
+    { ... }:
+    let localProxyPort = 43; in
+    {
+      security.apparmor.enable = true;
+
+      services.dnscrypt-proxy2.enable = true;
+      services.dnscrypt-proxy2.localPort = localProxyPort;
+
+      services.dnsmasq.enable = true;
+      services.dnsmasq.servers = [ "127.0.0.1#${toString localProxyPort}" ];
+    };
+  };
+
+  testScript = ''
+    $client->waitForUnit("dnsmasq");
+
+    # The daemon is socket activated; sending a single ping should activate it.
+    $client->fail("systemctl is-active dnscrypt-proxy2");
+    $client->execute("${pkgs.iputils}/bin/ping -c1 example.com");
+    $client->waitUntilSucceeds("systemctl is-active dnscrypt-proxy2");
+  '';
+})
